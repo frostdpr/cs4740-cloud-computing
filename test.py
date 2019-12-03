497913f830c45f02d8203886c0e9e34bc948be45
Christopher Truong <frostdpr@users.noreply.github.com>
2019-12-03 07:43:59 +0000

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import urllib.request
-import json
-import requests
-temp = 78
-url = 'http://ec2-54-80-48-194.compute-1.amazonaws.com:5000/governor/api/v1.0/current-setpoint/'
-data = json.dumps({'setPoint':str(temp)})
-response = requests.put(url, data=data)
